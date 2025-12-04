2177ea22bf14b642e8d1755fcdd86e61b1694c84
Arman0058 <mmohd2959@gmail.com>
2025-12-04 13:42:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4000; // use 4000 internally
 
 app.get("/", (req, res) => {
- res.send("Hello from CI/CD on EC2 🚀 " + new Date().toISOString());
-
+  res.send("Hello from CI/CD on EC2 🚀 " + new Date().toISOString());
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
